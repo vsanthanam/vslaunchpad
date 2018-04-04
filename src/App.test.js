919b9e8e5fa3ab4697627605e26a4e2d28daa448
Varun Santhanam <talkto@vsanthanam.com>
2018-04-04 14:15:54 -0700

    quickfix

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 
-const div = document.createElement('div');
-const app = <App />;
-
-it('is sane', () => {
+it('isnt insane', () => {
 
   expect(true).toEqual(true);
 
